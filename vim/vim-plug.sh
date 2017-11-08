6fa3151123e0bb8489f39d6594c50a5adeb272eb
Peter Benjamin <petermbenjamin@gmail.com>
2017-11-08 11:33:21 -0800

    Add vim-plug installer

--- vim/vim-plug.sh
+++ vim/vim-plug.sh
@@ -4,5 +4,9 @@ set -e
 
 echo "## INSTALLING VIM-PLUG..."
 
+# VIM
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# NEOVIM
+curl -fLo ~/.config/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
