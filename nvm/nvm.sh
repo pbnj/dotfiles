4ccb48801495cbee801077f047e1a4446facf686
Peter Benjamin <petermbenjamin@gmail.com>
2017-11-14 14:44:22 -0800

    Update nvm

--- nvm/nvm.sh
+++ nvm/nvm.sh
@@ -2,21 +2,7 @@
 
 set -e
 
-echo "## INSTALLING NVM ##"
-git clone https://github.com/creationix/nvm.git $HOME/.nvm
+echo "## INSTALLING NVM"
+# git clone https://github.com/creationix/nvm.git $HOME/.nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
 
-echo "## CONFIGURING ENV VARS ##"
-echo '
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-'
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-
-echo "## TESTING NVM INSTALLATION ##"
-nvm ls-remote
-
-echo "## INSTALLING LATEST NODE ##"
-nvm install node
-npm i -g npm
