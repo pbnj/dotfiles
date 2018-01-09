2abe34f18b24979ae9adb63f1795b0cb080766e7
Peter Benjamin <petermbenjamin@gmail.com>
2018-01-08 16:24:15 -0800

    update oh my zsh installer

--- zsh/oh_my_zsh.sh
+++ zsh/oh_my_zsh.sh
@@ -2,6 +2,3 @@
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# zsh syntax highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
