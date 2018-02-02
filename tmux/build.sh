1c62a4bfc5f49ff788755b9db5667eac7dd16472
Peter Benjamin <petermbenjamin@gmail.com>
2018-02-02 14:24:40 -0800

    Add tmux build/install scripts

---
+++ tmux/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+git clone https://github.com/tmux/tmux $HOME
+
+cd $HOME/tmux
+
+command -v yum > /dev/null 2>&1 && sudo yum install -y libevent-devel ncurses-devel
+command -v apt-get > /dev/null 2>&1 && sudo apt-get install -y libevent-dev ncurses-dev
+
+sh autogen.sh
+./configure && make && sudo make install
+sudo mv ./tmux /usr/local/bin/tmux
