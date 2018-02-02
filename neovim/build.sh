75ea6285e496d2a34b894c3055716b6782b7672c
Peter Benjamin <petermbenjamin@gmail.com>
2018-02-02 14:17:53 -0800

    Minor update

--- neovim/build.sh
+++ neovim/build.sh
@@ -9,4 +9,4 @@ cd $HOME/neovim
 command -v apt-get > /dev/null 2>&1 && sudo apt-get install -y ninja-build libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
 command -v yum > /dev/null 2>&1 && sudo yum -y install ninja-build libtool autoconf automake cmake gcc gcc-c++ make pkgconfig unzip
 
-make
+make && sudo make install
