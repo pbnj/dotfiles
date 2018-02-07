d19e3d09414e725dbdc65914e3d024ebeb8a4dff
Peter Benjamin <petermbenjamin@gmail.com>
2018-02-06 20:31:22 -0800

    Add fish configs

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,61 +1,30 @@
-# name: clearance
-# ---------------
-# Based on idan. Display the following bits on the left:
-# - Virtualenv name (if applicable, see https://github.com/adambrenecki/virtualfish)
-# - Current directory name
-# - Git branch and dirty state (if inside a git repo)
+# vim: set ft=sh:
 
-function _git_branch_name
-  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-end
+set -l normal (set_color normal)
+set -l red (set_color red)
+set -l normal (set_color normal)
 
-function _git_is_dirty
-  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
-end
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
 
 function fish_prompt
   set -l last_status $status
 
-  set -l cyan (set_color cyan)
-  set -l yellow (set_color yellow)
-  set -l red (set_color red)
-  set -l blue (set_color blue)
-  set -l green (set_color green)
-  set -l normal (set_color normal)
-
-  set -l cwd $blue(pwd | sed "s:^$HOME:~:")
-
-  # Output the prompt, left to right
-
-  # Add a newline before new prompts
   echo -e ''
-
-  # Display [venvname] if in a virtualenv
-  if set -q VIRTUAL_ENV
-      echo -n -s (set_color -b cyan black) '[' (basename "$VIRTUAL_ENV") ']' $normal ' '
-  end
-
-  # Print pwd or full path
-  echo -n -s $cwd $normal
-
-  # Show git branch and status
-  if [ (_git_branch_name) ]
-    set -l git_branch (_git_branch_name)
-
-    if [ (_git_is_dirty) ]
-      set git_info '(' $yellow $git_branch "±" $normal ')'
-    else
-      set git_info '(' $green $git_branch $normal ')'
-    end
-    echo -n -s ' · ' $git_info $normal
-  end
+  echo -n -s (whoami)@(hostname | cut -d . -f 1):(set_color $fish_color_cwd)(prompt_pwd)(set_color normal)(__fish_git_prompt)
 
   set -l prompt_color $red
   if test $last_status = 0
     set prompt_color $normal
   end
 
-  # Terminate with a nice prompt char
   echo -e ''
-  echo -e -n -s $prompt_color '⟩ ' $normal
+  echo -e -n -s $prompt_color '$ ' $normal
 end
+
