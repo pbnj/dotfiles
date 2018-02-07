9bc99c80dd6e606e7fe239ccc76bbbd863fdb1a2
Peter Benjamin <petermbenjamin@gmail.com>
2018-02-06 20:45:39 -0800

    Update fish_prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,14 +1,11 @@
 # vim: set ft=sh:
 
-set -l normal (set_color normal)
-set -l red (set_color red)
-set -l normal (set_color normal)
-
 # Fish git prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_show_informative_status 'yes'
 set __fish_git_prompt_color_branch yellow
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
@@ -17,7 +14,8 @@ function fish_prompt
   set -l last_status $status
 
   echo -e ''
-  echo -n -s (whoami)@(hostname | cut -d . -f 1):(set_color $fish_color_cwd)(prompt_pwd)(set_color normal)(__fish_git_prompt)
+  printf '%s@%s:%s%s%s%s' (whoami) (hostname) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (__fish_git_prompt)
+  # echo -n -s (whoami)@(hostname | cut -d . -f 1):(set_color $fish_color_cwd)(prompt_pwd)(set_color normal)(__fish_git_prompt)
 
   set -l prompt_color $red
   if test $last_status = 0
