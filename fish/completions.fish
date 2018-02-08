86d3a7d113c4fcc915e5ae7836bb4e4d5df2f772
Peter Benjamin <petermbenjamin@gmail.com>
2018-02-08 14:42:59 -0800

    Update fish completions

--- fish/completions.fish
+++ fish/completions.fish
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env fish
 
 mkdir -p ~/.config/fish/completions/
 
 # kubectl fish completions
 git clone https://github.com/evanlucas/fish-kubectl-completions ~/.config/fish/fish-kubectl-completions/
+
 cp ~/.config/fish/fish-kubectl-completions/kubectl.fish ~/.config/fish/completions/kubectl.fish
 
+rm -rf fish-kubectl-completions
