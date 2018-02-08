5248c6e4860ae0aa98d4b9feaa894c118ee713dd
Peter Benjamin <petermbenjamin@gmail.com>
2018-02-08 03:32:25 -0800

    Load ssh keys

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,43 @@
 set -x GOPATH $HOME
 set -x PATH $PATH /usr/local/go/bin $GOPATH/bin
 
+# SSH
+setenv SSH_ENV $HOME/.ssh/environment
+function start_agent
+    echo "Initializing new SSH agent ..."
+    ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+    echo "succeeded"
+    chmod 600 $SSH_ENV
+    source $SSH_ENV > /dev/null
+    ssh-add
+end
+function test_identities
+    ssh-add -l | grep "The agent has no identities" > /dev/null
+    if [ $status -eq 0 ]
+        ssh-add
+        if [ $status -eq 2 ]
+            start_agent
+        end
+    end
+end
+
+if [ -n "$SSH_AGENT_PID" ]
+    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
+    if [ $status -eq 0 ]
+      test_identities
+    end
+else
+    if [ -f $SSH_ENV ]
+      source $SSH_ENV > /dev/null
+    end
+    ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
+    if [ $status -eq 0 ]
+      test_identities
+    else
+      start_agent
+    end
+end
+
 function l --wraps ls
   if type -q gls
     gls -alFh --group-directories-first --color=auto $argv
@@ -25,6 +62,8 @@ if type -q git
     abbr g git
     abbr ga git add
     abbr gaa git add --all
+    abbr gcl git clone
+    abbr gd git diff
     abbr gcmsg git commit -m
     abbr gco git checkout
     abbr gp git push
