aad96cbf8fec857715c02c61b108502b453a8bb3
Peter Benjamin <petermbenjamin@gmail.com>
2018-02-08 17:10:41 -0800

    Add fish/conf.d

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,71 +1,31 @@
-set -x GOPATH $HOME
-set -x PATH $PATH /usr/local/go/bin $GOPATH/bin
+# General
+set -x LANG "en_US.UTF-8"
+set -x LC_COLLATE "en_US.UTF-8"
+set -x LC_CTYPE "en_US.UTF-8"
+set -x LC_MESSAGES "en_US.UTF-8"
+set -x LC_MONETARY "en_US.UTF-8"
+set -x LC_NUMERIC "en_US.UTF-8"
+set -x LC_TIME "en_US.UTF-8"
+set -x LC_ALL "en_US.UTF-8"
 
-# SSH
-setenv SSH_ENV $HOME/.ssh/environment
-function start_agent
-    echo "Initializing new SSH agent ..."
-    ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
-    echo "succeeded"
-    chmod 600 $SSH_ENV
-    source $SSH_ENV > /dev/null
-    ssh-add
-end
-function test_identities
-    ssh-add -l | grep "The agent has no identities" > /dev/null
-    if [ $status -eq 0 ]
-        ssh-add
-        if [ $status -eq 2 ]
-            start_agent
-        end
-    end
-end
-
-if [ -n "$SSH_AGENT_PID" ]
-    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
-    if [ $status -eq 0 ]
-      test_identities
-    end
-else
-    if [ -f $SSH_ENV ]
-      source $SSH_ENV > /dev/null
-    end
-    ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
-    if [ $status -eq 0 ]
-      test_identities
-    else
-      start_agent
-    end
-end
+# fish snippets (e.g. conf.d) supported as of fish >=2.3.0.
+# for older fish shells, uncomment lines below
+# for file in ~/.config/fish/conf.d/*.fish
+#  source $file
+# end
 
 function l --wraps ls
-  if type -q gls
-    gls -alFh --group-directories-first --color=auto $argv
+  if [ (uname) = "Darwin" ]
+    if type -q gls
+      gls -alFh --group-directories-first --color=auto $argv
+    else
+      if type -q brew
+        brew install coreutils
+      else
+        echo "Install coreutils for mac"
+      end
+    end
   else
     ls -alFh --group-directories-first --color=auto $argv
   end
-end
-
-# Kubectl aliases/functions
-if type -q kubectl
-    abbr k "kubectl"
-    abbr kgp "kubectl get pod"
-    abbr kc "kube config"
-end
-
-# Git aliases/functions
-if type -q git
-    if type -q hub
-      eval (hub alias -s)
-    end
-
-    abbr g git
-    abbr ga git add
-    abbr gaa git add --all
-    abbr gcl git clone
-    abbr gd git diff
-    abbr gcmsg git commit -m
-    abbr gco git checkout
-    abbr gp git push
-    abbr gst git status
 end
