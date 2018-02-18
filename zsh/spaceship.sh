6251f258a667dde58b1c8142f630d38b12b758f8
Peter Benjamin <petermbenjamin@gmail.com>
2018-02-18 10:03:34 -0800

    Fix script

--- zsh/spaceship.sh
+++ zsh/spaceship.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -e
+
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
