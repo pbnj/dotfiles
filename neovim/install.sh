e04e7d86ecbdfb6acd19f498659aeb1a89803edf
Peter Benjamin <petermbenjamin@gmail.com>
2018-02-25 21:38:11 -0800

    Update configs

--- neovim/install.sh
+++ neovim/install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-set -e
+set -ex
+
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+mv nvim.appimage $HOME
+chmod u+x $HOME/nvim.appimage
 
 mkdir -p $HOME/.config/nvim/
-cp ../.config/nvim/init.vim $HOME/.config/nvim/init.vim
+cp ./init.vim $HOME/.config/nvim/init.vim
 
 nvim +PlugInstall
