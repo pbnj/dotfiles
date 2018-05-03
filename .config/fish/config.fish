28a2d68c4574d03292200b7f83e86bd17d4e4061
Peter Benjamin <petermbenjamin@gmail.com>
2018-05-02 21:13:15 -0700

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,35 +1,28 @@
-# General
+#############
+## General ##
+#############
 set -x LANG "en_US.UTF-8"
+set -x LC_ALL "en_US.UTF-8"
 set -x LC_COLLATE "en_US.UTF-8"
 set -x LC_CTYPE "en_US.UTF-8"
 set -x LC_MESSAGES "en_US.UTF-8"
 set -x LC_MONETARY "en_US.UTF-8"
 set -x LC_NUMERIC "en_US.UTF-8"
 set -x LC_TIME "en_US.UTF-8"
-set -x LC_ALL "en_US.UTF-8"
 
-# Theme
+###########
+## Theme ##
+###########
 ## omf/theme-bobthefish
-set -g theme_nerd_font yes
+# set -g theme_nerd_font yes
 
-# fish snippets (e.g. conf.d) supported as of fish >=2.3.0.
-# for older fish shells, uncomment lines below
+## fish snippets (e.g. conf.d) supported as of fish >=2.3.0.
+## for older fish shells, uncomment lines below
 # for file in ~/.config/fish/conf.d/*.fish
 #  source $file
 # end
 
-function l --wraps ls
-  if [ (uname) = "Darwin" ]
-    if type -q gls
-      gls -alFh --group-directories-first --color=auto $argv
-    else
-      if type -q brew
-        brew install coreutils
-      else
-        echo "Install coreutils for mac"
-      end
-    end
-  else
-    ls -alFh --group-directories-first --color=auto $argv
-  end
-end
+###########
+# Aliases #
+###########
+alias l="ls -alFh"
