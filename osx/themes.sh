ab1e33b161daffb7daa8b54235de5dd5a5dad0c3
Peter Benjamin <petermbenjamin@gmail.com>
2018-05-17 10:17:47 -0700

    update osx themes

--- osx/themes.sh
+++ osx/themes.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-CURDIR="$(dirname $0)"
+set -ex
 
-open ${CURDIR}/themes/nord*
+# iTerm Themes
+ITERM_THEME_DIR="$HOME/iterm/themes"
+mkdir -p "$ITERM_THEME_DIR"
+git clone https://github.com/MartinSeeler/iterm2-material-design $ITERM_THEME_DIR/material-design
+git clone https://github.com/chriskempson/base16-iterm2 $ITERM_THEME_DIR/base16
+git clone https://github.com/dracula/iterm $ITERM_THEME_DIR/dracula
 
+# Terminal.app Themes
+mkdir -p $HOME/terminal-app/themes
