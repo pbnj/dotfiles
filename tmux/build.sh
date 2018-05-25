d0b6adef87b5f8f9a6f01fa59a7dcbb21fedde56
Peter Benjamin <petermbenjamin@gmail.com>
2018-05-25 15:06:11 -0700

    Update tmux build script

--- tmux/build.sh
+++ tmux/build.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-git clone https://github.com/tmux/tmux $HOME/tmux
+TMUX_HOME="$HOME/tmux"
+git clone https://github.com/tmux/tmux "$TMUX_HOME"
 
-cd $HOME/tmux
+cd "$TMUX_HOME"
 
-command -v yum > /dev/null 2>&1 && sudo yum install -y libevent-devel ncurses-devel
-command -v apt-get > /dev/null 2>&1 && sudo apt-get install -y libevent-dev ncurses-dev
+command -v yum > /dev/null 2>&1 && sudo yum update -y && sudo yum install -y libevent-devel ncurses-devel
+command -v apt-get > /dev/null 2>&1 && sudo apt-get update && sudo apt-get install -y libevent-dev ncurses-dev
 
-sh autogen.sh
-./configure && make && sudo make install
+sh autogen.sh && ./configure && make && sudo make install
 sudo mv ./tmux /usr/local/bin/tmux
