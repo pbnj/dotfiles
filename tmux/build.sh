174c0f00fe8a85bb7efe4f2e1ffca76c2833f022
Peter Benjamin <petermbenjamin@gmail.com>
2018-06-12 22:19:29 -0700

    Add brew support

--- tmux/build.sh
+++ tmux/build.sh
@@ -5,10 +5,10 @@ set -e
 TMUX_HOME="$HOME/tmux"
 git clone https://github.com/tmux/tmux "$TMUX_HOME"
 
+command -v yum &>/dev/null && sudo yum update -y && sudo yum install -y libevent-devel ncurses-devel
+command -v apt-get &>/dev/null && sudo apt-get update && sudo apt-get install -y libevent-dev ncurses-dev
+command -v brew &>/dev/null && brew install tmux && exit 0
+
 cd "$TMUX_HOME"
-
-command -v yum > /dev/null 2>&1 && sudo yum update -y && sudo yum install -y libevent-devel ncurses-devel
-command -v apt-get > /dev/null 2>&1 && sudo apt-get update && sudo apt-get install -y libevent-dev ncurses-dev
-
 sh autogen.sh && ./configure && make && sudo make install
 sudo mv ./tmux /usr/local/bin/tmux
