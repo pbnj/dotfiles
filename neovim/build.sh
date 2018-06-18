64c17a1641f8a314d28a85f8a6f529dc50fa5261
Peter Benjamin <petermbenjamin@gmail.com>
2018-06-18 15:10:03 -0700

    Fix neovim build script

--- neovim/build.sh
+++ neovim/build.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-git clone https://github.com/neovim/neovim $HOME/neovim
+git clone https://github.com/neovim/neovim "$HOME/neovim"
 
-cd $HOME/neovim
+cd "$HOME/neovim"
 
 command -v apt-get > /dev/null 2>&1 && sudo apt-get install -y ninja-build libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
 command -v yum > /dev/null 2>&1 && sudo yum -y install ninja-build libtool autoconf automake cmake gcc gcc-c++ make pkgconfig unzip
