c1b79620c8467a38334ccb5beb7de30733c376a9
Peter Benjamin <petermbenjamin@gmail.com>
2018-07-12 15:16:55 -0700

    Fix script

--- kubernetes/kubeadm.sh
+++ kubernetes/kubeadm.sh
@@ -10,15 +10,15 @@ if command -v yum &>/dev/null; then
 	sudo systemctl enable docker
 	sudo systemctl start docker
 
-	sudo cat <<EOF >/etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
+	sudo -s 'cat <<EOF >/etc/yum.repos.d/kubernetes.repo
+	[kubernetes]
+	name=Kubernetes
+	baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+	enabled=1
+	gpgcheck=1
+	repo_gpgcheck=1
+	gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+	EOF'
 
 	setenforce 0
 	sudo yum install -y kubelet kubeadm kubectl
