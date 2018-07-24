504d594e4b5a6a7a5456931f8392f99e69b8cf4d
Peter Benjamin <petermbenjamin@gmail.com>
2018-07-23 21:31:28 -0700

    Minor

--- git/install.sh
+++ git/install.sh
@@ -5,6 +5,6 @@ set -x
 
 sudo apt-add-repository ppa:git-core/ppa
 sudo apt-get update
-sudo apt-get install git
+sudo apt-get install git -y
 
 ln -sf $HOME/.dotfiles/git/.gitconfig $HOME/.gitconfig
