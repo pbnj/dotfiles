f2adcd04c3156b1b3a0363cb16c051ba4e6b05f0
Peter Benjamin <petermbenjamin@gmail.com>
2018-07-23 17:09:58 -0700

    Build vim

--- vim/build.sh
+++ vim/build.sh
@@ -16,7 +16,8 @@ sudo rm -rf /usr/local/share/vim /usr/bin/vim
 git clone https://github.com/vim/vim "$HOME/vim"
 (
 cd "$HOME/vim"
-git pull && git fetch
+git pull
+git fetch
 cd src
 make distclean
 cd ..
@@ -24,12 +25,7 @@ cd ..
 ./configure \
   --enable-multibyte \
   --enable-perlinterp=dynamic \
-  --enable-rubyinterp=dynamic \
-  --with-ruby-command=/usr/local/bin/ruby \
-  --enable-pythoninterp=dynamic \
-  --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
   --enable-python3interp \
-  --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
   --enable-luainterp \
   --with-luajit \
   --enable-cscope \
@@ -39,10 +35,11 @@ cd ..
   --enable-fontset \
   --enable-largefile \
   --disable-netbeans \
-  --with-compiledby="yourname" \
-  --enable-fail-if-missing
+  --with-compiledby="Peter Benjamin" \
+  --enable-fail-if-missing \
+  --prefix=/usr/local/bin
 
-make && sudo make install
+make
+sudo make install
 )
-
 
