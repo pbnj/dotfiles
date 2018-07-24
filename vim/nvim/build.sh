a7aec3c47b87ae550569ef060ff4b6d3df358db1
Peter Benjamin <petermbenjamin@gmail.com>
2018-07-24 15:09:26 -0700

    clean whitespaces

--- vim/nvim/build.sh
+++ vim/nvim/build.sh
@@ -3,19 +3,45 @@
 set -e
 set -x
 
-git clone https://github.com/neovim/neovim "$HOME/neovim"
-
-(
-cd "$HOME/neovim"
-
 if command -v apt-get &>/dev/null; then
-  sudo apt-get install -y ninja-build libtool autoconf automake cmake make g++ pkg-config unzip python3 python3-pip
+    sudo apt-get install -y \
+        autoconf \
+        automake \
+        cmake \
+        g++ \
+        libtool \
+        make \
+        ninja-build \
+        pkg-config \
+        python3 \
+        python3-pip \
+        unzip
 fi
 
 if command -v yum &>/dev/null; then
-  sudo yum -y install ninja-build libtool autoconf automake cmake gcc gcc-c++ make pkgconfig unzip python3 python3-pip
+    sudo yum -y install \
+         autoconf \
+         automake \
+         cmake \
+         gcc \
+         gcc-c++ \
+         libtool \
+         make \
+         ninja-build \
+         pkgconfig \
+         python34 \
+         python34-pip \
+         unzip
 fi
 
-make
-sudo make install
+git clone https://github.com/neovim/neovim "$HOME/neovim"
+
+(
+    cd "$HOME/neovim"
+    make
+    sudo make install
+    rm -rf "$HOME/neovim"
 )
+
+pip3 install --upgarde pip --user
+pip3 install --upgrade neovim --user
