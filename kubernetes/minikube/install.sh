5546d11a0c518b4350a8b3e468b66288187f9853
Peter Benjamin <petermbenjamin@gmail.com>
2018-07-30 14:06:26 -0700

    Add support of (un)installing minikube)

--- kubernetes/minikube/install.sh
+++ kubernetes/minikube/install.sh
@@ -3,15 +3,17 @@
 set -e
 set -x
 
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
-# curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl
+sudo curl -Lo /usr/bin/minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 
+sudo chmod +x /usr/bin/minikube
+sudo curl -Lo /usr/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl 
+sudo chmod +x /usr/bin/kubectl
 
 # export MINIKUBE_WANTUPDATENOTIFICATION=false
-# export MINIKUBE_WANTREPORTERRORPROMPT=false
+export MINIKUBE_WANTREPORTERRORPROMPT=false
 export MINIKUBE_HOME=$HOME
 export CHANGE_MINIKUBE_NONE_USER=true
 mkdir -p $HOME/.kube
 touch $HOME/.kube/config
 
 export KUBECONFIG=$HOME/.kube/config
-sudo -E ./minikube start --vm-driver=none
+sudo -E minikube start --vm-driver=none
