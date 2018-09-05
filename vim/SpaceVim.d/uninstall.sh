a943a5ded27a1c63e85296013b733cada1f5ce9a
Peter Benjamin <petermbenjamin@gmail.com>
2018-09-05 11:30:19 -0700

    Add uninstaller

---
+++ vim/SpaceVim.d/uninstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+set -x
+
+curl -sLf https://spacevim.org/install.sh | bash -s -- uninstall
+
+rm -rf ~/.vim
+rm -rf ~/.SpaceVim
+rm -rf ~/.SpaceVim.d
+rm -rf ~/.cache/SpaceVim
+rm -rf ~/.cache/vimfiles
