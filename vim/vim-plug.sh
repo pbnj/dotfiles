0a91dcde46d5bc52eb1cfc4426ddd3836914bafd
Peter Benjamin <petermbenjamin@gmail.com>
2018-10-15 15:08:33 -0700

    Simplify vim-plug installer script

--- vim/vim-plug.sh
+++ vim/vim-plug.sh
@@ -4,8 +4,3 @@ set -e
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-if command -v nvim > /dev/null 2>&1; then
-
-    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-fi
