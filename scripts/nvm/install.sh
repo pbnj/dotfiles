30640d3e4233133718b85d520325ead6b91967e2
Peter Benjamin <petermbenjamin@gmail.com>
2018-10-31 23:43:34 -0700

    Reorg

--- scripts/nvm/install.sh
+++ scripts/nvm/install.sh
@@ -3,8 +3,4 @@
 set -e
 set -x
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-
-nvm install --lts
-
-echo "\tsource $HOME/.bash_profile"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
