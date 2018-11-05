d13f2d345d5fc19d002ce986ed2b58ff30bf1d49
Peter Benjamin <petermbenjamin@gmail.com>
2018-11-05 09:50:32 -0800

    Fix vim build

--- scripts/vim/build.sh
+++ scripts/vim/build.sh
@@ -3,43 +3,48 @@
 set -e
 set -x
 
-if command -v apt &>/dev/null
-then
-  sudo apt-get remove --purge vim vim-runtime vim-gnome vim-tiny vim-gui-common
-  sudo apt-get install liblua5.1-dev luajit libluajit-5.1 python-dev ruby-dev libperl-dev libncurses5-dev libatk1.0-dev libx11-dev libxpm-dev libxt-dev
-  #Optional: so vim can be uninstalled again via `dpkg -r vim`
-  sudo apt-get install checkinstall
+if command -v apt &>/dev/null; then
+	sudo apt-get remove --purge vim vim-runtime vim-gnome vim-tiny vim-gui-common
+	sudo apt-get install liblua5.1-dev luajit libluajit-5.1 python-dev ruby-dev libperl-dev libncurses5-dev libatk1.0-dev libx11-dev libxpm-dev libxt-dev
+	#Optional: so vim can be uninstalled again via `dpkg -r vim`
+	sudo apt-get install checkinstall
+fi
+
+if command -v yum &>/dev/null; then
+	sudo yum update
+	sudo yum groupinstall 'Development tools'
+	sudo yum install -y ncurses ncurses-devel
 fi
 
 sudo rm -rf /usr/local/share/vim /usr/bin/vim
 
 git clone https://github.com/vim/vim "$HOME/vim"
 (
-cd "$HOME/vim"
-git pull
-git fetch
-cd src
-make distclean
-cd ..
+	cd "$HOME/vim"
+	git pull
+	git fetch
+	cd src
+	make distclean
+	cd ..
 
-./configure \
-  --enable-multibyte \
-  --enable-perlinterp=dynamic \
-  --enable-python3interp \
-  --enable-luainterp \
-  --with-luajit \
-  --enable-cscope \
-  --enable-gui=auto \
-  --with-features=huge \
-  --with-x \
-  --enable-fontset \
-  --enable-largefile \
-  --disable-netbeans \
-  --with-compiledby="Peter Benjamin" \
-  --enable-fail-if-missing \
-  --prefix=/usr/local/bin
+	./configure \
+		--disable-netbeans \
+		--enable-cscope \
+		--enable-fail-if-missing \
+		--enable-fontset \
+		--enable-gui=auto \
+		--enable-largefile \
+		--enable-luainterp \
+		--enable-multibyte \
+		--enable-perlinterp=dynamic \
+		--enable-python3interp \
+		--prefix=/usr/local/bin \
+		--with-compiledby="Peter Benjamin" \
+		--with-features=huge \
+		--with-luajit \
+		--with-tlib=ncurses \
+		--without-x
 
-make
-sudo make install
+	make
+	sudo make install
 )
-
