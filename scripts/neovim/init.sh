356db3163b4d4482b524e4c63f34c64816e066c4
Peter Benjamin <petermbenjamin@gmail.com>
2018-11-08 10:14:18 -0800

    Fix neovim init script

--- scripts/neovim/init.sh
+++ scripts/neovim/init.sh
@@ -3,30 +3,30 @@
 set -e
 set -x
 
-mkdir -p ~/.dotfiles/neovim/.config/nvim/pack/pb/start/
+mkdir -p ~/.dotfiles/neovim/.config/nvim/pack/bundles/start/
 
-git clone https://github.com/airblade/vim-gitgutter       ~/.dotfiles/neovim/.config/nvim/pack/pb/start/vim-gitgutter
-git clone https://github.com/cespare/vim-toml             ~/.dotfiles/neovim/.config/nvim/pack/pb/start/vim-toml
-git clone https://github.com/ekalinin/Dockerfile.vim      ~/.dotfiles/neovim/.config/nvim/pack/pb/start/Dockerfile.vim
-git clone https://github.com/elzr/vim-json                ~/.dotfiles/neovim/.config/nvim/pack/pb/start/vim-json
-git clone https://github.com/ervandew/supertab            ~/.dotfiles/neovim/.config/nvim/pack/pb/start/supertab
-git clone https://github.com/fatih/vim-go                 ~/.dotfiles/neovim/.config/nvim/pack/pb/start/vim-go
-git clone https://github.com/godlygeek/tabular            ~/.dotfiles/neovim/.config/nvim/pack/pb/start/tabular
-git clone https://github.com/hashivim/vim-hashicorp-tools ~/.dotfiles/neovim/.config/nvim/pack/pb/start/vim-hashicorp-tools
-git clone https://github.com/junegunn/fzf                 ~/.dotfiles/neovim/.config/nvim/pack/pb/start/fzf
-git clone https://github.com/junegunn/fzf.vim             ~/.dotfiles/neovim/.config/nvim/pack/pb/start/fzf.vim
-git clone https://github.com/mattn/emmet-vim              ~/.dotfiles/neovim/.config/nvim/pack/pb/start/emmet-vim
-git clone https://github.com/mzlogin/vim-markdown-toc     ~/.dotfiles/neovim/.config/nvim/pack/pb/start/vim-markdown-toc
-git clone https://github.com/plasticboy/vim-markdown      ~/.dotfiles/neovim/.config/nvim/pack/pb/start/vim-markdown
-git clone https://github.com/tpope/vim-apathy             ~/.dotfiles/neovim/.config/nvim/pack/pb/start/vim-apathy
-git clone https://github.com/tpope/vim-commentary         ~/.dotfiles/neovim/.config/nvim/pack/pb/start/vim-commentary
-git clone https://github.com/tpope/vim-dadbod             ~/.dotfiles/neovim/.config/nvim/pack/pb/start/vim-dadbod
-git clone https://github.com/tpope/vim-dispatch           ~/.dotfiles/neovim/.config/nvim/pack/pb/start/vim-dispatch
-git clone https://github.com/tpope/vim-fugitive           ~/.dotfiles/neovim/.config/nvim/pack/pb/start/vim-fugitive
-git clone https://github.com/tpope/vim-sensible           ~/.dotfiles/neovim/.config/nvim/pack/pb/start/vim-sensible
-git clone https://github.com/tpope/vim-sleuth             ~/.dotfiles/neovim/.config/nvim/pack/pb/start/vim-sleuth
-git clone https://github.com/tpope/vim-surround           ~/.dotfiles/neovim/.config/nvim/pack/pb/start/vim-surround
-git clone https://github.com/tpope/vim-vinegar            ~/.dotfiles/neovim/.config/nvim/pack/pb/start/vim-vinegar
-git clone https://github.com/vim-airline/vim-airline      ~/.dotfiles/neovim/.config/nvim/pack/pb/start/vim-airline
-git clone https://github.com/vim-syntastic/syntastic      ~/.dotfiles/neovim/.config/nvim/pack/pb/start/syntastic
-git clone https://github.com/w0rp/ale                     ~/.dotfiles/neovim/.config/nvim/pack/pb/start/ale
+git submodule add https://github.com/airblade/vim-gitgutter       neovim/.config/nvim/pack/bundles/start/vim-gitgutter
+git submodule add https://github.com/cespare/vim-toml             neovim/.config/nvim/pack/bundles/start/vim-toml
+git submodule add https://github.com/ekalinin/Dockerfile.vim      neovim/.config/nvim/pack/bundles/start/Dockerfile.vim
+git submodule add https://github.com/elzr/vim-json                neovim/.config/nvim/pack/bundles/start/vim-json
+git submodule add https://github.com/ervandew/supertab            neovim/.config/nvim/pack/bundles/start/supertab
+git submodule add https://github.com/fatih/vim-go                 neovim/.config/nvim/pack/bundles/start/vim-go
+git submodule add https://github.com/godlygeek/tabular            neovim/.config/nvim/pack/bundles/start/tabular
+git submodule add https://github.com/hashivim/vim-hashicorp-tools neovim/.config/nvim/pack/bundles/start/vim-hashicorp-tools
+git submodule add https://github.com/junegunn/fzf                 neovim/.config/nvim/pack/bundles/start/fzf
+git submodule add https://github.com/junegunn/fzf.vim             neovim/.config/nvim/pack/bundles/start/fzf.vim
+git submodule add https://github.com/mattn/emmet-vim              neovim/.config/nvim/pack/bundles/start/emmet-vim
+git submodule add https://github.com/mzlogin/vim-markdown-toc     neovim/.config/nvim/pack/bundles/start/vim-markdown-toc
+git submodule add https://github.com/plasticboy/vim-markdown      neovim/.config/nvim/pack/bundles/start/vim-markdown
+git submodule add https://github.com/tpope/vim-apathy             neovim/.config/nvim/pack/bundles/start/vim-apathy
+git submodule add https://github.com/tpope/vim-commentary         neovim/.config/nvim/pack/bundles/start/vim-commentary
+git submodule add https://github.com/tpope/vim-dadbod             neovim/.config/nvim/pack/bundles/start/vim-dadbod
+git submodule add https://github.com/tpope/vim-dispatch           neovim/.config/nvim/pack/bundles/start/vim-dispatch
+git submodule add https://github.com/tpope/vim-fugitive           neovim/.config/nvim/pack/bundles/start/vim-fugitive
+git submodule add https://github.com/tpope/vim-sensible           neovim/.config/nvim/pack/bundles/start/vim-sensible
+git submodule add https://github.com/tpope/vim-sleuth             neovim/.config/nvim/pack/bundles/start/vim-sleuth
+git submodule add https://github.com/tpope/vim-surround           neovim/.config/nvim/pack/bundles/start/vim-surround
+git submodule add https://github.com/tpope/vim-vinegar            neovim/.config/nvim/pack/bundles/start/vim-vinegar
+git submodule add https://github.com/vim-airline/vim-airline      neovim/.config/nvim/pack/bundles/start/vim-airline
+git submodule add https://github.com/vim-syntastic/syntastic      neovim/.config/nvim/pack/bundles/start/syntastic
+git submodule add https://github.com/w0rp/ale                     neovim/.config/nvim/pack/bundles/start/ale
