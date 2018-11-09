3b3f993293166499ee662b587fcf0e1e2ef358dc
Peter Benjamin <petermbenjamin@gmail.com>
2018-11-09 15:58:35 -0800

    Reorg scripts

--- scripts/langs/go/install-pkgs.sh
+++ scripts/langs/go/install-pkgs.sh
@@ -7,7 +7,7 @@ set -x
 # go get -u -v github.com/petermbenjamin/go-open/cmd/gopen
 go get -u -v github.com/goreleaser/goreleaser
 go get -u -v github.com/jessfraz/weather
+go get -u -v github.com/mvdan/sh
 go get -u -v github.com/petermbenjamin/go-gitignore/cmd/gitignore
 go get -u -v github.com/tj/go-search
 go get -u -v golang.org/x/vgo
-go get -u -v mvdan.cc/sh/cmd/shfmt
