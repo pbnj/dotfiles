884f27c6953eeb38318cb902d41710386639d37f
Peter Benjamin <petermbenjamin@gmail.com>
2018-11-21 14:57:59 -0800

    Improve spacevim install script

--- scripts/tools/spacevim/install.sh
+++ scripts/tools/spacevim/install.sh
@@ -4,3 +4,10 @@ set -e
 set -x
 
 curl -sLf https://spacevim.org/install.sh | bash
+rm -rf "$HOME/.vimrc"
+
+if command -v pip3 &> /dev/null; then
+  pip3 install --user --upgrade neovim
+else
+  pip install --user --upgrade neovim
+fi
