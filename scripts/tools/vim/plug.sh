874cc26493f3d11794efbf253aa344a6db3c70a1
Peter Benjamin <6811830+petermbenjamin@users.noreply.github.com>
2018-11-26 12:30:56 -0800

    Update plug.sh

--- scripts/tools/vim/plug.sh
+++ scripts/tools/vim/plug.sh
@@ -7,5 +7,5 @@
 set -e
 set -x
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
