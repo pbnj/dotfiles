256dccd836fa0ece8d4f32bcc5b6b11e8af0c0db
Peter Benjamin <petermbenjamin@gmail.com>
2019-04-22 17:17:06 -0700

    Fix neovim package path

--- scripts/tools/vim/packages.sh
+++ scripts/tools/vim/packages.sh
@@ -37,23 +37,23 @@ git submodule add https://github.com/vim-airline/vim-airline      vim/.vim/pack/
 # rm -Rf .git/modules/vim/pack/shapeshed/start/vim-airline
 # git commit
 
-git submodule add https://github.com/airblade/vim-gitgutter 	  nvim/.local/share/nvim/petermbenjamin/start/vim-gitgutter
-git submodule add https://github.com/cespare/vim-toml       	  nvim/.local/share/nvim/petermbenjamin/start/vim-toml
-git submodule add https://github.com/ekalinin/Dockerfile.vim      nvim/.local/share/nvim/petermbenjamin/start/Dockerfile.vim
-git submodule add https://github.com/elzr/vim-json                nvim/.local/share/nvim/petermbenjamin/start/vim-json
-git submodule add https://github.com/ervandew/supertab            nvim/.local/share/nvim/petermbenjamin/start/supertab
-git submodule add https://github.com/fatih/vim-go                 nvim/.local/share/nvim/petermbenjamin/start/vim-go
-git submodule add https://github.com/godlygeek/tabular            nvim/.local/share/nvim/petermbenjamin/start/tabular
-git submodule add https://github.com/hashivim/vim-hashicorp-tools nvim/.local/share/nvim/petermbenjamin/start/vim-hashicorp-tools
-git submodule add https://github.com/mzlogin/vim-markdown-toc     nvim/.local/share/nvim/petermbenjamin/start/vim-markdown-toc
-git submodule add https://github.com/tpope/vim-apathy             nvim/.local/share/nvim/petermbenjamin/start/vim-apathy
-git submodule add https://github.com/tpope/vim-commentary         nvim/.local/share/nvim/petermbenjamin/start/vim-commentary
-git submodule add https://github.com/tpope/vim-dadbod             nvim/.local/share/nvim/petermbenjamin/start/vim-dadbod
-git submodule add https://github.com/tpope/vim-dispatch           nvim/.local/share/nvim/petermbenjamin/start/vim-dispatch
-git submodule add https://github.com/tpope/vim-fugitive           nvim/.local/share/nvim/petermbenjamin/start/vim-fugitive
-git submodule add https://github.com/tpope/vim-sensible           nvim/.local/share/nvim/petermbenjamin/start/vim-sensible
-git submodule add https://github.com/tpope/vim-sleuth             nvim/.local/share/nvim/petermbenjamin/start/vim-sleuth
-git submodule add https://github.com/tpope/vim-surround           nvim/.local/share/nvim/petermbenjamin/start/vim-surround
-git submodule add https://github.com/tpope/vim-vinegar            nvim/.local/share/nvim/petermbenjamin/start/vim-vinegar
-git submodule add https://github.com/vim-airline/vim-airline      nvim/.local/share/nvim/petermbenjamin/start/vim-airline
+git submodule add https://github.com/airblade/vim-gitgutter 	  nvim/.local/share/nvim/site/pack/petermbenjamin/start/vim-gitgutter
+git submodule add https://github.com/cespare/vim-toml       	  nvim/.local/share/nvim/site/pack/petermbenjamin/start/vim-toml
+git submodule add https://github.com/ekalinin/Dockerfile.vim      nvim/.local/share/nvim/site/pack/petermbenjamin/start/Dockerfile.vim
+git submodule add https://github.com/elzr/vim-json                nvim/.local/share/nvim/site/pack/petermbenjamin/start/vim-json
+git submodule add https://github.com/ervandew/supertab            nvim/.local/share/nvim/site/pack/petermbenjamin/start/supertab
+git submodule add https://github.com/fatih/vim-go                 nvim/.local/share/nvim/site/pack/petermbenjamin/start/vim-go
+git submodule add https://github.com/godlygeek/tabular            nvim/.local/share/nvim/site/pack/petermbenjamin/start/tabular
+git submodule add https://github.com/hashivim/vim-hashicorp-tools nvim/.local/share/nvim/site/pack/petermbenjamin/start/vim-hashicorp-tools
+git submodule add https://github.com/mzlogin/vim-markdown-toc     nvim/.local/share/nvim/site/pack/petermbenjamin/start/vim-markdown-toc
+git submodule add https://github.com/tpope/vim-apathy             nvim/.local/share/nvim/site/pack/petermbenjamin/start/vim-apathy
+git submodule add https://github.com/tpope/vim-commentary         nvim/.local/share/nvim/site/pack/petermbenjamin/start/vim-commentary
+git submodule add https://github.com/tpope/vim-dadbod             nvim/.local/share/nvim/site/pack/petermbenjamin/start/vim-dadbod
+git submodule add https://github.com/tpope/vim-dispatch           nvim/.local/share/nvim/site/pack/petermbenjamin/start/vim-dispatch
+git submodule add https://github.com/tpope/vim-fugitive           nvim/.local/share/nvim/site/pack/petermbenjamin/start/vim-fugitive
+git submodule add https://github.com/tpope/vim-sensible           nvim/.local/share/nvim/site/pack/petermbenjamin/start/vim-sensible
+git submodule add https://github.com/tpope/vim-sleuth             nvim/.local/share/nvim/site/pack/petermbenjamin/start/vim-sleuth
+git submodule add https://github.com/tpope/vim-surround           nvim/.local/share/nvim/site/pack/petermbenjamin/start/vim-surround
+git submodule add https://github.com/tpope/vim-vinegar            nvim/.local/share/nvim/site/pack/petermbenjamin/start/vim-vinegar
+git submodule add https://github.com/vim-airline/vim-airline      nvim/.local/share/nvim/site/pack/petermbenjamin/start/vim-airline
 
