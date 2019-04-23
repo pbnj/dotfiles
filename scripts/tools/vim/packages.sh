618b2b13248f6c2ffd0c1ead392a225f23ebd006
Peter Benjamin <petermbenjamin@gmail.com>
2019-04-22 17:04:34 -0700

    Update (neo)vim submodules

--- scripts/tools/vim/packages.sh
+++ scripts/tools/vim/packages.sh
@@ -7,28 +7,25 @@ set -x
 # Packages under this path will be autoloaded
 # For more info: https://shapeshed.com/vim-packages/
 
-mkdir -p $HOME/.vim/pack/pb/start
-
-git submodule add https://github.com/airblade/vim-gitgutter 	  $HOME/.dotfiles/vim/.vim/pack/pb/start/vim-gitgutter
-git submodule add https://github.com/cespare/vim-toml       	  $HOME/.dotfiles/vim/.vim/pack/pb/start/vim-toml
-git submodule add https://github.com/ekalinin/Dockerfile.vim      $HOME/.dotfiles/vim/.vim/pack/pb/start/Dockerfile.vim
-git submodule add https://github.com/elzr/vim-json                $HOME/.dotfiles/vim/.vim/pack/pb/start/vim-json
-git submodule add https://github.com/ervandew/supertab            $HOME/.dotfiles/vim/.vim/pack/pb/start/supertab
-git submodule add https://github.com/fatih/vim-go                 $HOME/.dotfiles/vim/.vim/pack/pb/start/vim-go
-git submodule add https://github.com/godlygeek/tabular            $HOME/.dotfiles/vim/.vim/pack/pb/start/tabular
-git submodule add https://github.com/hashivim/vim-hashicorp-tools $HOME/.dotfiles/vim/.vim/pack/pb/start/vim-hashicorp-tools
-git submodule add https://github.com/mzlogin/vim-markdown-toc     $HOME/.dotfiles/vim/.vim/pack/pb/start/vim-markdown-toc
-git submodule add https://github.com/plasticboy/vim-markdown      $HOME/.dotfiles/vim/.vim/pack/pb/start/vim-markdown
-git submodule add https://github.com/tpope/vim-apathy             $HOME/.dotfiles/vim/.vim/pack/pb/start/vim-apathy
-git submodule add https://github.com/tpope/vim-commentary         $HOME/.dotfiles/vim/.vim/pack/pb/start/vim-commentary
-git submodule add https://github.com/tpope/vim-dadbod             $HOME/.dotfiles/vim/.vim/pack/pb/start/vim-dadbod
-git submodule add https://github.com/tpope/vim-dispatch           $HOME/.dotfiles/vim/.vim/pack/pb/start/vim-dispatch
-git submodule add https://github.com/tpope/vim-fugitive           $HOME/.dotfiles/vim/.vim/pack/pb/start/vim-fugitive
-git submodule add https://github.com/tpope/vim-sensible           $HOME/.dotfiles/vim/.vim/pack/pb/start/vim-sensible
-git submodule add https://github.com/tpope/vim-sleuth             $HOME/.dotfiles/vim/.vim/pack/pb/start/vim-sleuth
-git submodule add https://github.com/tpope/vim-surround           $HOME/.dotfiles/vim/.vim/pack/pb/start/vim-surround
-git submodule add https://github.com/tpope/vim-vinegar            $HOME/.dotfiles/vim/.vim/pack/pb/start/vim-vinegar
-git submodule add https://github.com/vim-airline/vim-airline      $HOME/.dotfiles/vim/.vim/pack/pb/start/vim-airline
+git submodule add https://github.com/airblade/vim-gitgutter 	  vim/.vim/pack/petermbenjamin/start/vim-gitgutter
+git submodule add https://github.com/cespare/vim-toml       	  vim/.vim/pack/petermbenjamin/start/vim-toml
+git submodule add https://github.com/ekalinin/Dockerfile.vim      vim/.vim/pack/petermbenjamin/start/Dockerfile.vim
+git submodule add https://github.com/elzr/vim-json                vim/.vim/pack/petermbenjamin/start/vim-json
+git submodule add https://github.com/ervandew/supertab            vim/.vim/pack/petermbenjamin/start/supertab
+git submodule add https://github.com/fatih/vim-go                 vim/.vim/pack/petermbenjamin/start/vim-go
+git submodule add https://github.com/godlygeek/tabular            vim/.vim/pack/petermbenjamin/start/tabular
+git submodule add https://github.com/hashivim/vim-hashicorp-tools vim/.vim/pack/petermbenjamin/start/vim-hashicorp-tools
+git submodule add https://github.com/mzlogin/vim-markdown-toc     vim/.vim/pack/petermbenjamin/start/vim-markdown-toc
+git submodule add https://github.com/tpope/vim-apathy             vim/.vim/pack/petermbenjamin/start/vim-apathy
+git submodule add https://github.com/tpope/vim-commentary         vim/.vim/pack/petermbenjamin/start/vim-commentary
+git submodule add https://github.com/tpope/vim-dadbod             vim/.vim/pack/petermbenjamin/start/vim-dadbod
+git submodule add https://github.com/tpope/vim-dispatch           vim/.vim/pack/petermbenjamin/start/vim-dispatch
+git submodule add https://github.com/tpope/vim-fugitive           vim/.vim/pack/petermbenjamin/start/vim-fugitive
+git submodule add https://github.com/tpope/vim-sensible           vim/.vim/pack/petermbenjamin/start/vim-sensible
+git submodule add https://github.com/tpope/vim-sleuth             vim/.vim/pack/petermbenjamin/start/vim-sleuth
+git submodule add https://github.com/tpope/vim-surround           vim/.vim/pack/petermbenjamin/start/vim-surround
+git submodule add https://github.com/tpope/vim-vinegar            vim/.vim/pack/petermbenjamin/start/vim-vinegar
+git submodule add https://github.com/vim-airline/vim-airline      vim/.vim/pack/petermbenjamin/start/vim-airline
 
 # updating packages
 # git submodule update --remote --merge
@@ -39,3 +36,24 @@ git submodule add https://github.com/vim-airline/vim-airline      $HOME/.dotfile
 # git rm vim/pack/shapeshed/start/vim-airline
 # rm -Rf .git/modules/vim/pack/shapeshed/start/vim-airline
 # git commit
+
+git submodule add https://github.com/airblade/vim-gitgutter 	  nvim/.local/share/nvim/petermbenjamin/start/vim-gitgutter
+git submodule add https://github.com/cespare/vim-toml       	  nvim/.local/share/nvim/petermbenjamin/start/vim-toml
+git submodule add https://github.com/ekalinin/Dockerfile.vim      nvim/.local/share/nvim/petermbenjamin/start/Dockerfile.vim
+git submodule add https://github.com/elzr/vim-json                nvim/.local/share/nvim/petermbenjamin/start/vim-json
+git submodule add https://github.com/ervandew/supertab            nvim/.local/share/nvim/petermbenjamin/start/supertab
+git submodule add https://github.com/fatih/vim-go                 nvim/.local/share/nvim/petermbenjamin/start/vim-go
+git submodule add https://github.com/godlygeek/tabular            nvim/.local/share/nvim/petermbenjamin/start/tabular
+git submodule add https://github.com/hashivim/vim-hashicorp-tools nvim/.local/share/nvim/petermbenjamin/start/vim-hashicorp-tools
+git submodule add https://github.com/mzlogin/vim-markdown-toc     nvim/.local/share/nvim/petermbenjamin/start/vim-markdown-toc
+git submodule add https://github.com/tpope/vim-apathy             nvim/.local/share/nvim/petermbenjamin/start/vim-apathy
+git submodule add https://github.com/tpope/vim-commentary         nvim/.local/share/nvim/petermbenjamin/start/vim-commentary
+git submodule add https://github.com/tpope/vim-dadbod             nvim/.local/share/nvim/petermbenjamin/start/vim-dadbod
+git submodule add https://github.com/tpope/vim-dispatch           nvim/.local/share/nvim/petermbenjamin/start/vim-dispatch
+git submodule add https://github.com/tpope/vim-fugitive           nvim/.local/share/nvim/petermbenjamin/start/vim-fugitive
+git submodule add https://github.com/tpope/vim-sensible           nvim/.local/share/nvim/petermbenjamin/start/vim-sensible
+git submodule add https://github.com/tpope/vim-sleuth             nvim/.local/share/nvim/petermbenjamin/start/vim-sleuth
+git submodule add https://github.com/tpope/vim-surround           nvim/.local/share/nvim/petermbenjamin/start/vim-surround
+git submodule add https://github.com/tpope/vim-vinegar            nvim/.local/share/nvim/petermbenjamin/start/vim-vinegar
+git submodule add https://github.com/vim-airline/vim-airline      nvim/.local/share/nvim/petermbenjamin/start/vim-airline
+
