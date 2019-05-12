f12edd9b2c19942f81d1c72c9f16f8a53569d61d
Peter Benjamin <petermbenjamin@gmail.com>
2019-05-12 11:01:28 -0700

    Install langs via makefile

--- langs.mk
+++ langs.mk
@@ -1,10 +1,16 @@
 .PHONY: rust
 rust: ## Install rust toolchain
 	curl https://sh.rustup.rs -sSf | sh -s -- -y
-	sh scripts/langs/rust/toolchain.sh
-	sh scripts/langs/rust/cargo.sh
+	rustup component add rls rust-analysis rust-src
+	rustup component add clippy rustfmt rust-docs
+	rustup toolchain add nightly
 	rustup completions bash > /usr/local/etc/bash_completion.d/rustup
 	rustup completions bash cargo > /usr/local/etc/bash_completion.d/cargo
+
+.PHONY: crates
+crates: ## Install crates
+	cargo +nightly install rust-racer
+	cargo install exa bat ripgrep
 
 .PHONY: go
 GO_VERSION := $(shell curl -fsSL https://golang.org/VERSION?m=text)
@@ -13,8 +19,18 @@ GO_ARCH := linux-amd64
 go: ## Install go
 	curl -fsSL https://dl.google.com/go/$(GO_VERSION).$(GO_ARCH).tar.gz | sudo tar -vxzC /usr/local -f -
 
+.PHONY: go-pkgs
+go-pkgs: ## Install GO packages
+	go get -u -v golang.org/x/tools/cmd/gopls
+	GO111MODULE=on go get -u -v sigs.k8s.io/kind@master
+
 .PHONY: nvm
 nvm: ## Install Node Version Manager and Node
 ifeq (, $(shell which nvm))
 	sh $(CURDIR)/scripts/langs/nvm/install.sh
 endif
+	nvm install --lts
+
+.PHONY: npm
+npm: ## Install npm packages
+	npm install -g prettier
--- scripts/langs/rust/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-set -x
-
-curl https://sh.rustup.rs -sSf | sh
