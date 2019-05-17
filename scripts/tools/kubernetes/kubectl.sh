b88c7952c3492a57af5ba6e779a35ea3fa760a01
Peter Benjamin <petermbenjamin@gmail.com>
2019-05-16 22:05:13 -0700

    Improve kubectl installer

--- scripts/tools/kubernetes/kubectl.sh
+++ scripts/tools/kubernetes/kubectl.sh
@@ -3,23 +3,13 @@
 set -e
 set -x
 
-if command -v apt-get &> /dev/null; then
-	sudo apt-get update && sudo apt-get install -y apt-transport-https
-	curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-	echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-	sudo apt-get update
-	sudo apt-get install -y kubectl
+VERSION="$1"
+OS="$(uname | tr '[:upper:]' '[:lower:]')"
+
+if [ -z "$VERSION" ]; then
+	VERSION="$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
 fi
 
-if command -v yum &> /dev/null; then
-	cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
-	yum install -y kubectl
-fi
+curl -LO https://storage.googleapis.com/kubernetes-release/release/"$VERSION"/bin/"$OS"/amd64/kubectl
+chmod +x kubectl
+
