c5e77af9ad8a412fcdb8bb3b9a47848b79802a57
Peter Benjamin <petermbenjamin@gmail.com>
2019-05-16 22:21:13 -0700

    Document variables

--- scripts/tools/kubernetes/kubectl.sh
+++ scripts/tools/kubernetes/kubectl.sh
@@ -3,7 +3,9 @@
 set -e
 set -x
 
+# VERSION is expected to be in "vX.Y.Z" format
 VERSION="$1"
+# OS is tested with "darwin" and "linux"
 OS="$(uname | tr '[:upper:]' '[:lower:]')"
 
 if [ -z "$VERSION" ]; then
@@ -12,4 +14,3 @@ fi
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/"$VERSION"/bin/"$OS"/amd64/kubectl
 chmod +x kubectl
-
