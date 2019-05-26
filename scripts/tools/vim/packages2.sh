d7952445f633e6df67a555ab1d7f51e1106eb32e
Peter Benjamin <petermbenjamin@gmail.com>
2019-05-25 20:14:22 -0700

    Add submodules

---
+++ scripts/tools/vim/packages2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+set -x
+
+git submodule add https://github.com/airblade/vim-gitgutter		vim/vim/pack/plugins/start/vim-gitgutter
+git submodule add https://github.com/cespare/vim-toml			vim/vim/pack/plugins/start/vim-toml
+git submodule add https://github.com/Chiel92/vim-autoformat		vim/vim/pack/plugins/start/vim-autoformat
+git submodule add https://github.com/ekalinin/Dockerfile.vim		vim/vim/pack/plugins/start/Dockerfile.vim
+git submodule add https://github.com/elzr/vim-json			vim/vim/pack/plugins/start/vim-json
+git submodule add https://github.com/fatih/vim-go			vim/vim/pack/plugins/start/vim-go
+git submodule add https://github.com/godlygeek/tabular			vim/vim/pack/plugins/start/tabular
+git submodule add https://github.com/hashivim/vim-hashicorp-tools	vim/vim/pack/plugins/start/vim-hashicorp-tools
+git submodule add https://github.com/itchyny/calendar.vim		vim/vim/pack/plugins/start/calendar.vim
+git submodule add https://github.com/itchyny/lightline.vim		vim/vim/pack/plugins/start/lightline.vim
+git submodule add https://github.com/junegunn/fzf			vim/vim/pack/plugins/start/fzf
+git submodule add https://github.com/majutsushi/tagbar			vim/vim/pack/plugins/start/tagbar
+git submodule add https://github.com/mzlogin/vim-markdown-toc		vim/vim/pack/plugins/start/vim-markdown-toc
+git submodule add https://github.com/natebosch/vim-lsc			vim/vim/pack/plugins/start/vim-lsc
+git submodule add https://github.com/plasticboy/vim-markdown		vim/vim/pack/plugins/start/vim-markdown
+git submodule add https://github.com/Quramy/vison			vim/vim/pack/plugins/start/vison
+git submodule add https://github.com/racer-rust/vim-racer		vim/vim/pack/plugins/start/vim-racer
+git submodule add https://github.com/rust-lang/rust.vim			vim/vim/pack/plugins/start/rust.vim
+git submodule add https://github.com/tpope/vim-apathy			vim/vim/pack/plugins/start/vim-apathy
+git submodule add https://github.com/tpope/vim-commentary		vim/vim/pack/plugins/start/vim-commentary
+git submodule add https://github.com/tpope/vim-dadbod			vim/vim/pack/plugins/start/vim-dadbod
+git submodule add https://github.com/tpope/vim-dispatch			vim/vim/pack/plugins/start/vim-dispatch
+git submodule add https://github.com/tpope/vim-fugitive			vim/vim/pack/plugins/start/vim-fugitive
+git submodule add https://github.com/tpope/vim-git			vim/vim/pack/plugins/start/vim-git
+git submodule add https://github.com/tpope/vim-rhubarb			vim/vim/pack/plugins/start/vim-rhubarb
+git submodule add https://github.com/tpope/vim-sensible			vim/vim/pack/plugins/start/vim-sensible
+git submodule add https://github.com/tpope/vim-surround			vim/vim/pack/plugins/start/vim-surround
+git submodule add https://github.com/tpope/vim-unimpaired		vim/vim/pack/plugins/start/vim-unimpaired
+git submodule add https://github.com/tpope/vim-vinegar			vim/vim/pack/plugins/start/vim-vinegar
+git submodule add https://github.com/tsandall/vim-rego			vim/vim/pack/plugins/start/vim-rego
+git submodule add https://github.com/uarun/vim-protobuf			vim/vim/pack/plugins/start/vim-protobuf
