c5ba4c2c560fed845b680665bf1a15f9cf3a6601
Peter Benjamin <petermbenjamin@gmail.com>
2019-05-30 16:19:49 -0700

    Go installer

--- langs.mk
+++ langs.mk
@@ -18,11 +18,8 @@ gimme: ## Install gimme (Go version manager)
 	sudo chmod +x /usr/local/bin/gimme
 
 .PHONY: go
-GO_VERSION := $(shell curl -fsSL https://golang.org/VERSION?m=text)
-GO_ARCH := linux-amd64
-#https://dl.google.com/go/go1.11.5.linux-amd64.tar.gz
 go: ## Install go
-	curl -fsSL https://dl.google.com/go/$(GO_VERSION).$(GO_ARCH).tar.gz | sudo tar -vxzC /usr/local -f -
+	sh $(CURDIR)/scripts/langs/go/install.sh
 
 .PHONY: go-pkgs
 go-pkgs: ## Install Go packages
