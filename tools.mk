4d77919d156a32bc2237af1f7e614c906d580c1f
Peter Benjamin <petermbenjamin@gmail.com>
2019-06-05 23:05:58 -0700

    Add oh-my-zsh targets

--- tools.mk
+++ tools.mk
@@ -152,6 +152,19 @@ neovim-install: ## Install neovim
 	pip3 install neovim
 
 ####################
+# ZSH
+####################
+
+.PHONY: oh-my-zsh
+oh-my-zsh: ## Install zsh
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+.PHONY: zsh-plugins
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+####################
 # STOW
 ####################
 
