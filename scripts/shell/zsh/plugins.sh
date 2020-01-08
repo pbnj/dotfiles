6268f2cd91824a0337aa64c48770d528a262f0da
Peter Benjamin <petermbenjamin@gmail.com>
2020-01-07 20:58:26 -0800

    Create rust completions plugins

--- scripts/shell/zsh/plugins.sh
+++ scripts/shell/zsh/plugins.sh
@@ -7,3 +7,7 @@ git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zs
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+mkdir -p ~/.oh-my-zsh/custom/plugins/rustup && rustup completions zsh > ~/.oh-my-zsh/custom/plugins/rustup/_rustup
+mkdir -p ~/.oh-my-zsh/custom/plugins/cargo && rustup completions zsh cargo > ~/.oh-my-zsh/custom/plugins/cargo/_cargo
+
+[[ -f "$HOME/.zcompdump*" ]] && rm -rf "$HOME/.zcompdump*"
