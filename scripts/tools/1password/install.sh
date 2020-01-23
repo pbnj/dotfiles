0324a008ed9edb1b32268b5da728e19d13b8e114
Peter Benjamin <petermbenjamin@gmail.com>
2020-01-23 22:09:09 +0000

    Add prompts

--- scripts/tools/1password/install.sh
+++ scripts/tools/1password/install.sh
@@ -3,12 +3,15 @@
 set -e
 set -x
 
-OP_VERSION="0.6.1"
-OP_ARCH="arm"
-if [[ "$(lscpu | grep Architecture | awk '{print $2}')" == "x86_64" ]]; then
-	OP_ARCH="amd64"
+if ! command -v enquirer &>/dev/null; then
+	echo "enquirer not installed. See https://github.com/termapps/enquirer"
+	exit 1
 fi
 
-OP_ZIP="op_linux_${OP_ARCH}_v${OP_VERSION}.zip"
+OP_VERSION="$(enquirer input --message "Version" -d "0.8.0")"
+OP_PLATFORM="$(enquirer select --message "Platform" linux darwin windows)"
+OP_ARCH="$(enquirer select --message "Architecture" amd64 arm)"
+
+OP_ZIP="op_${OP_PLATFORM}_${OP_ARCH}_v${OP_VERSION}.zip"
 
 curl -fsSL -o $HOME/$OP_ZIP https://cache.agilebits.com/dist/1P/op/pkg/v$OP_VERSION/$OP_ZIP
