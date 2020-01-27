2121ca9d8c6ba769d25136fba6fa13844a10bf5e
Peter Benjamin <petermbenjamin@gmail.com>
2020-01-27 19:36:45 +0000

    Update 1password installer

--- scripts/tools/1password/install.sh
+++ scripts/tools/1password/install.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 set -e
-set -x
 
-if ! command -v enquirer &>/dev/null; then
-	echo "enquirer not installed. See https://github.com/termapps/enquirer"
-	exit 1
-fi
+DEFAULT_VERSION="0.9.0"
+DEFAULT_PLATFORM="$(uname | tr '[:upper:]' '[:lower:]')"
+DEFAULT_ARCH="$(uname -m | sed -e 's/x86_64/amd64/g')"
 
-OP_VERSION="$(enquirer input --message "Version" -d "0.8.0")"
-OP_PLATFORM="$(enquirer select --message "Platform" linux darwin windows)"
-OP_ARCH="$(enquirer select --message "Architecture" amd64 arm)"
+read -p "Version [${DEFAULT_VERSION}]: " OP_VERSION
+read -p "Platform [${DEFAULT_PLATFORM}]: " OP_PLATFORM
+read -p "Arch [${DEFAULT_ARCH//x86_64/amd64}]: " OP_ARCH
+
+OP_VERSION=${OP_VERSION:-$DEFAULT_VERSION}
+OP_PLATFORM=${OP_PLATFORM:-$DEFAULT_PLATFORM}
+OP_ARCH=${OP_ARCH:-$DEFAULT_ARCH}
 
 OP_ZIP="op_${OP_PLATFORM}_${OP_ARCH}_v${OP_VERSION}.zip"
+OP_PATH="${HOME}/${OP_ZIP}"
 
+echo "Downloading ${OP_ZIP}"
 curl -fsSL -o $HOME/$OP_ZIP https://cache.agilebits.com/dist/1P/op/pkg/v$OP_VERSION/$OP_ZIP
+
+if command -v unzip &>/dev/null; then
+	echo "Unzipping ${OP_ZIP}"
+	mkdir -p ${HOME}/bin
+	unzip ${OP_PATH} op -d ${HOME}/bin
+	rm -rf "${OP_PATH}"
+fi
