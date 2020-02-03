697db7d1f88c760b18352cc5e318811eb340d666
Peter Benjamin <petermbenjamin@gmail.com>
2020-02-03 10:51:37 -0800

    Add tools

--- scripts/langs/go/install-pkgs.sh
+++ scripts/langs/go/install-pkgs.sh
@@ -9,5 +9,9 @@ go get -u -v sigs.k8s.io/kind@v0.7.0
 go get -u -v golang.org/x/tools/cmd/goimports
 go get -u -v golang.org/x/tools/gopls@latest
 go get -u -v golang.org/x/lint/golint
-go get -u -v github.com/pbnj/go-gitignore/...
+go get -u -v github.com/pbnj/gitignore
 go get -u -v github.com/tj/go-search
+go get -u -v github.com/josharian/impl
+go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
+go get -u -v github.com/fatih/gomodifytags
+go get -u -v github.com/cweill/gotests/...
