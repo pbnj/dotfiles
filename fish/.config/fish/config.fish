07d4788a26401091e77f36b6bd1264d8a948c8bd
Peter Benjamin <petermbenjamin@gmail.com>
2020-02-13 15:00:32 -0800

    Add fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,34 @@
+# set -g theme_color_scheme light
+# set -g theme_date_format "+%A, %d %b %Y %l:%M %p"
+# set -g theme_display_docker_machine no
+# set -g theme_display_hg yes
+# set -g theme_display_hostname ssh
+# set -g theme_display_nvm yes
+# set -g theme_display_ruby no
+# set -g theme_display_user ssh
+# set -g theme_display_virtualenv no
+
+set -g default_user your_normal_user
+set -g fish_prompt_pwd_dir_length 0
+set -g theme_avoid_ambiguous_glyphs yes
+set -g theme_display_cmd_duration no
+set -g theme_display_date no
+set -g theme_display_git yes
+set -g theme_display_git_ahead_verbose yes
+set -g theme_display_git_dirty yes
+set -g theme_display_git_dirty_verbose yes
+set -g theme_display_git_master_branch yes
+set -g theme_display_git_stashed_verbose yes
+set -g theme_display_git_untracked yes
+set -g theme_display_jobs_verbose yes
+set -g theme_display_k8s_context yes
+set -g theme_display_vagrant yes
+set -g theme_display_vi no
+set -g theme_git_worktree_support no
+set -g theme_nerd_fonts yes
+set -g theme_newline_cursor yes
+set -g theme_newline_prompt '$ '
+set -g theme_powerline_fonts yes
+set -g theme_project_dir_length 1
+set -g theme_show_exit_status yes
+set -g theme_use_abbreviated_branch_name yes
